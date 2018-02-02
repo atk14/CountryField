a402173597cfbaa392ce9dc2a47d352f76140657
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-02-02 16:13:22 +0100

    Test fixed

--- test/tc_country_list_loader.php
+++ test/tc_country_list_loader.php
@@ -4,12 +4,12 @@ class TcCountryListLoader extends TcBase {
 	function test(){
 		putenv("LANG=cs_CZ.UTF-8");
 		$list = CountryListLoader::Get();
-		$this->assertEquals("Česká republika",$list["CZ"]);
+		$this->assertEquals("Slovensko",$list["SK"]);
 		$this->assertEquals("Spojené státy",$list["US"]);
 
 		putenv("LANG=en_US.UTF-8");
 		$list = CountryListLoader::Get();
-		$this->assertEquals("Czech Republic",$list["CZ"]);
+		$this->assertEquals("Slovakia",$list["SK"]);
 		$this->assertEquals("United States",$list["US"]);
 	}
 }
