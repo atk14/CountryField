58b7efcc976177b0a2f7aa9d471b799d2ad97fcc
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-02-02 16:10:29 +0100

    Test fixed

--- test/tc_to_country_name.php
+++ test/tc_to_country_name.php
@@ -3,12 +3,12 @@ class TcToCountryName extends TcBase {
 
 	function test(){
 		putenv("LANG=cs_CZ.UTF-8");
-		$this->assertEquals("Česká republika",smarty_modifier_to_country_name("CZ"));
+		$this->assertEquals("Slovensko",smarty_modifier_to_country_name("SK"));
 		$this->assertEquals("Spojené státy",smarty_modifier_to_country_name("US"));
 		$this->assertEquals("XX",smarty_modifier_to_country_name("XX"));
 
 		putenv("LANG=en_US.UTF-8");
-		$this->assertEquals("Czech Republic",smarty_modifier_to_country_name("CZ"));
+		$this->assertEquals("Slovakia",smarty_modifier_to_country_name("SK"));
 		$this->assertEquals("United States",smarty_modifier_to_country_name("US"));
 		$this->assertEquals("XX",smarty_modifier_to_country_name("XX"));
 	}
